863da888c93150cc15d5abd0e5745c99027fbc2a
santoshshinde2012 <santoshshinde2012@gmail.com>
2015-07-24 16:47:06 +0530

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+angular.module('contactsApp', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller('contactsCtrl', function($scope, $cordovaContacts, $ionicLoading){
+ 
+  $scope.getContacts = function() {
+	$ionicLoading.show({
+			template: 'Loading...'
+	});
+		
+    $scope.phoneContacts = [];
+
+    function onSuccess(contacts) {
+      for (var i = 0; i < contacts.length; i++) {
+        var contact = contacts[i];
+	      $scope.phoneContacts.push(contact);
+      }
+	  $ionicLoading.hide();
+    };
+
+    function onError(contactError) {
+      alert(contactError);
+    };
+
+    var options = {};
+    options.multiple = true;
+    $cordovaContacts.find(options).then(onSuccess, onError);
+  };
+});
